19865266afb390507b73f9c98d1f5af8a2652c11
Zhen <zhenw.wang@gmail.com>
2014-11-16 17:41:33 -0500

    Month, day, year

--- main.py
+++ main.py
@@ -2,7 +2,22 @@ import webapp2
 
 form="""
 <form method="post">
-	<input name="q">
+	What is your birthday?
+	<br>
+	<label> Month
+		<input type="text" name="month">
+	</label>
+
+	<label> Day
+		<input type="text" name="day">
+	</label>
+
+	<label> Year
+		<input type="text" name="year">
+	</label>
+
+	<br>
+	<br>
 	<input type="submit">
 </form>
 """
