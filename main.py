98eb496928075ba225f4f99c2e0bd9305b9cbb03
Zhen <zhenw.wang@gmail.com>
2014-11-16 21:55:51 -0500

    Maintain input and escaping handling

--- main.py
+++ main.py
@@ -24,13 +24,42 @@ def valid_year(year):
 		if year > 1900 and year < 2020:
 			return year
 
+#"<b>%s</b>"%a
+#"text %s text %s" % (var1, var2)
+#"text % (NAME)s text" % {"NAME":value}
+
+#t1, t2 and t3 are global variables
+def sub1(s):
+	return t1%s
+
+def sub2(s1, s2):
+	return t2 % (s1, s2)
+
+def sub_m(name, nickname):
+	return t3 % {'name': name, 'nickname': nickname}
+
+#" -> &quot;
+#> -> &gt;
+#< -> &lt;
+#& -> &amp;
+
+import cgi
+def escape_html(s):
+	return cgi.escape(s, quote = True)
+#	for (i, o) in(("&", "&amp;"),
+#					(">", "&gt;"),
+#					("<", "&lt;"),
+#					('"', "&quot;")):
+#		s = s.replace(i, o)
+#	return s
+
 form="""
 <form method="post">
 	What is your birthday?
 	<br>
-	<label>Month <input type="text" name="month"></label>
-	<label>Day <input type="text" name="day"></label>
-	<label>Year <input type="text" name="year"></label>
+	<label>Month <input type="text" name="month" value="%(month)s"></label>
+	<label>Day <input type="text" name="day" value="%(day)s"></label>
+	<label>Year <input type="text" name="year" value="%(year)s"></label>
 	<div style="color: red">%(error)s</div>
 	<br>
 	<br>
@@ -40,18 +69,25 @@ form="""
 
 class MainPage(webapp2.RequestHandler):
 	def write_form(self, error="", month="", day="", year=""):
-		self.response.out.write(form % {"error": error})
+		self.response.out.write(form % {"error": error,
+										"month": escape_html(month),
+										"day": escape_html(day),
+										"year": escape_html(year)})
 
 	def get(self):
 		self.write_form()
 
 	def post(self):
-		user_month = valid_month(self.request.get('month'))
-		user_day = valid_day(self.request.get('day'))
-		user_year = valid_year(self.request.get('year'))
+		user_month = self.request.get('month')
+		user_day = self.request.get('day')
+		user_year = self.request.get('year')
 
-		if not (user_month and user_day and user_year):
-			self.write_form("Not valid")
+		month =  valid_month(user_month)
+		day = valid_day(user_day)
+		year = valid_year(user_year)
+
+		if not (month and day and year):
+			self.write_form("Not valid", user_month, user_day, user_year)
 		else:
 			self.response.out.write("Done")
 
